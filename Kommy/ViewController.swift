c897f3dc9dd0b4bfe58a6519a54743fb9e9e2767
Vladimir Ladygin <ladygin@lenvoku.ru>
2022-01-31 17:41:10 +0300

    Create UI, added a glossary

--- Kommy/ViewController.swift
+++ Kommy/ViewController.swift
@@ -8,7 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//Mark: Outlets
+    @IBOutlet weak var gensecImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    //Mark: Properties
+    let incorrectMovesAllowed = 5
+    var listOfWords = [
+        "Атеизм",
+                "Базис",
+                "Буржуазия",
+                "Буржуазная демократия",
+                "Демократический централизм",
+                "Диктатура пролетариата",
+                "Империализм",
+                "Интернационализм",
+                "Капитализм",
+                "Экспроприация",
+                "Капиталисты",
+                "Классовая борьба",
+                "Классы",
+                "Консенсунс",
+                "Коммунизм",
+                "Коммунистическая партия",
+                "Марксизм-ленинизм",
+                "Маркс",
+                "Ленин",
+                "Пролетариат",
+                "Ревизионизм",
+                "Симпозиум",
+                "Эксплуатация",
+                "Энгельс",
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
