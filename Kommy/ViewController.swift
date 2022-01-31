be4b4ed8a454b2d3f3b3a8d7b2032f1bb0b8afa8
Vladimir Ladygin <ladygin@lenvoku.ru>
2022-01-31 16:29:38 +0300

    Initial Commit

---
+++ Kommy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kommy
+//
+//  Created by Владимир Ладыгин on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
