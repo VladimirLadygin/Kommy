faf80d94b950bd3322598bc3284be3c5cd148bd2
Vladimir Ladygin <ladygin@lenvoku.ru>
2022-02-02 14:01:30 +0300

    Add Delay and Disable button after Win and Lose Add sad Brejnev after Lose Add App Icon

--- Kommy/ViewController.swift
+++ Kommy/ViewController.swift
@@ -46,23 +46,33 @@ class ViewController: UIViewController {
     
     var totalWins = 0 {
         didSet {
-            newRound()
+            delayWithSeconds(3) {
+                self.newRound()
+            }
         }
     }
     
     var totalLosses = 0 {
         didSet {
-            newRound()
+            delayWithSeconds(3) {
+                self.newRound()
+            }
         }
     }
     
     //Mark: Methods
+    func delayWithSeconds(_ seconds: Double, completion: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            completion()
+        }
+    }
+    
     func enableButtons( enable: Bool = true) {
         for button in letterButtons {
             button.isEnabled = enable
         }
     }
-
+    
     func newRound() {
         guard !listOfWords.isEmpty else {
             enableButtons(enable: false)
@@ -73,9 +83,15 @@ class ViewController: UIViewController {
         currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
         updateUI()
         enableButtons()
-
+        
         print(newWord,incorrectMovesAllowed)
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Do any additional setup after loading the view.
+        newRound()
+    }
+    
     func updateCorrectWordLabel() {
         var displayWord = [String] ()
         for letter in currentGame.guessedWord {
@@ -83,20 +99,22 @@ class ViewController: UIViewController {
         }
         correctWordLabel.text = displayWord.joined(separator: " ")
     }
-
-
+    
+    
     func updateState () {
-        print(#line, currentGame.guessedWord == currentGame.word)
-        if currentGame.incorrectMovesRemaining < 1 {
+        //        print(#line, currentGame.guessedWord == currentGame.word)
+        if currentGame.incorrectMovesRemaining <= 0 {
+            enableButtons(enable: false)
             totalLosses += 1
+            
         } else if currentGame.guessedWord.lowercased() == currentGame.word.lowercased() {
+            enableButtons(enable: false)
             totalWins += 1
-        } else  {
-            updateUI()
+            
         }
     }
-
-
+    
+    
     func updateUI() {
         let movesRemaining = currentGame.incorrectMovesRemaining
         let imageNumber = (movesRemaining + 6*6) % 6
@@ -104,24 +122,21 @@ class ViewController: UIViewController {
         gensecImageView.image = UIImage(named: image)
         updateCorrectWordLabel()
         scoreLabel.text = "Выиграно: \(totalWins), Проиграно: \(totalLosses)"
-//        print(movesRemaining,imageNumber,gensecImageView.image)
-
+        
+        updateState()
+        //        print(movesRemaining,imageNumber,gensecImageView.image)
+        
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //Do any additional setup after loading the view.
-        newRound()
-    }
     
     //Mark: IBAction
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
         let letter = sender.title(for: .normal)!
         currentGame.playerGuessed(letter: Character(letter))
-        updateState()
+        updateUI()
     }
-
+    
     
     
 }
